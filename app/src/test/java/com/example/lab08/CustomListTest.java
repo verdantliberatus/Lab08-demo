a6440a2c321aab4b24e818596b0cc62b3782cb06
verdantliberatus <verdant.liberatus@gmail.com>
2026-03-06 15:30:54 -0700

    failed countCities test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -25,4 +25,13 @@ public class CustomListTest {
         assertFalse(list.hasCity(calgary));
     }
 
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        assertEquals(0, list.countCities());
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertEquals(1, list.countCities());
+    }
+
 }
